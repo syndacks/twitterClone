f411f2860da64b643baece773395cebc24b46b3b
Dacks <math.milliken@gmail.com>
2016-12-31 04:22:18 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hello, world"
+    render text: "hello, world, what the fuck is going on!?"
   end
   
 end
